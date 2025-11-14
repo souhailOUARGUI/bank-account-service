872c9b38f7d5cfbfbb42450fc5620f6c4332f6ab
SouhailOUARGUI <ouargui550@gmail.com>
2025-11-14 12:22:51 +0100

    added rest endpoint auto generation with @RepositoryRestResource

--- src/main/java/org/example/bankaccountservice/repositories/BankAccountRepository.java
+++ src/main/java/org/example/bankaccountservice/repositories/BankAccountRepository.java
@@ -1,8 +1,14 @@
 package org.example.bankaccountservice.repositories;
 
 import org.example.bankaccountservice.entities.BankAccount;
+import org.example.bankaccountservice.enums.AccountType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
+
+@RepositoryRestResource
 public interface BankAccountRepository extends JpaRepository<BankAccount, String> {
-
+ List<BankAccount> findByType(AccountType type);
 }
